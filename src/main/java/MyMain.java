26c530fce6bea5ab8366f1fad9cef031f810309f
catcherindarah <22rray@athenian.org>
2021-01-12 06:28:13 +0000

    I tried for a little more than an hour, but I am still having problems

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,17 +1,43 @@
+import java.util.Random; 
 public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) { 
-        return null;
+        int a = 0;
+		int shuffled[] =new int[52];
+        for (int i = 0; i<25 ;i++)
+        {
+            a+=2;
+		}
+		a=a-a;
+        for (int i=26; i<arr.length;i++)
+        {
+		  shuffled[a]=arr[i];
+		  a+=2;
+		}
+        
+       
     }
 
     // Shuffles an array, using the selection shuffle algorithm
     public static int[] selectionShuffle(int[] arr) { 
-        return null;
-    }
+        int shuffled[] = new int[arr.length];
+		for(int k = 0; k<arr.length; k++){
+		  while(arr[k]!=0){
+		  int j =(int) Math.random()*arr.length;
+		  shuffled[k]=arr[j];
+		  arr[j]=0;
+    } 
+    
 
 
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        int[] values = [1, 2, 3, 4, 5, 21, 22, 23, 24, 25];
+		for (int j = 1; j <= 1; j++) {
+            perfectShuffle(values);
+            selectionShuffle(values);
+        }
+
+
     }
 }
